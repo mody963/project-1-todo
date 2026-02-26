62178d3050a7754eeda15d5c055bfbf133eaf1c0
FernandovanderGaag <1099893@hr.nl>
2026-02-26 12:03:37 +0100

    Main menu in arrow keys

--- view/ConsoleTaskView.cs
+++ view/ConsoleTaskView.cs
@@ -26,25 +26,79 @@ class ConsoleTaskView : ITaskView
     {
         while (true)
         {
-            DisplayTasks(_service.GetAllTasks());
+            
 
-            Console.WriteLine("\nOptions:");
-            Console.WriteLine("1. Add Task");
-            Console.WriteLine("2. Remove Task");
-            Console.WriteLine("3. Toggle Task State");
-            Console.WriteLine("4. List Tasks");
-            Console.WriteLine("5. Exit");
-
-            string option = Prompt("Select an option: ");
-
-            switch (option)
+            var main_options = new List<string>
             {
-                case "1":
+                "Add Task",
+                "Remove Task",
+                "Toggle Task State",
+                "List Tasks",
+                "Exit"
+            };
+
+            int select_index = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Main Menu ===\n");
+
+                DisplayTasks(_service.GetAllTasks());
+                Console.WriteLine();
+                for (int i = 0; i < main_options.Count; i++)
+                {
+                    if (i == select_index)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"> {main_options[i]}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {main_options[i]}");
+                    }
+                }
+
+                //Console.WriteLine("\nUse Arrow Keys to navigate, Enter to select");
+
+                var key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    select_index--;
+                    if (select_index < 0)
+                    {
+                        select_index = main_options.Count - 1;
+                    }
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    select_index++;
+                    if (select_index >= main_options.Count)
+                    {
+                        select_index = 0;
+                    }
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+            }
+
+            switch (select_index)
+            {
+                case 0:
                     string description = Prompt("Enter task description: ");
                     _service.AddTask(description);
                     break;
 
-                case "2":
+                case 1:
                     string removeIdStr = Prompt("Enter task id to remove: ");
                     if (int.TryParse(removeIdStr, out int removeId))
                     {
@@ -52,24 +106,19 @@ class ConsoleTaskView : ITaskView
                     }
                     break;
 
-                case "3":
+                case 2:
                     string toggleIdStr = Prompt("Enter task id to toggle: ");
                     if (int.TryParse(toggleIdStr, out int toggleId))
                     {
                         _service.ToggleTaskCompletion(toggleId);
                     }
                     break;
-                // case "4":
+                // case 3:
 
 
 
-                case "5":
+                case 4:
                     return;
-
-                default:
-                    Console.WriteLine("Invalid option. Press any key to continue...");
-                    Console.ReadKey();
-                    break;
             }
         }
     }
