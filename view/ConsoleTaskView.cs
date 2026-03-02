f3909fe40a2d777bb6a64433f2fd57de6f9378cc
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-03-02 12:11:30 +0100

    filter shit
    gekeken naar filter maar nog niet volledig werkend

--- view/ConsoleTaskView.cs
+++ view/ConsoleTaskView.cs
@@ -7,233 +7,159 @@ class ConsoleTaskView : ITaskView
         _service = service;
     }
 
-    // void DisplayTasks(IEnumerable<TaskItem> tasks)
-    // {
-    //     Console.Clear();
-    //     Console.WriteLine("==== ToDo List ====");
 
-    //     foreach (var task in tasks)
-    //         Console.WriteLine($"{task}");
-    // }
-    // void DisplayTasks(IMyCollection<TaskItem> tasks)
-    // {
-    //     Console.Clear();
-    //     Console.ForegroundColor = ConsoleColor.Cyan;
-    //     Console.WriteLine("==== ToDo List ====");
-    //     Console.ForegroundColor = ConsoleColor.White;
-
-    //     var iterator = tasks.GetIterator();
-
-    //     while (iterator.HasNext())
-    //     {
-    //         var task = iterator.Next();
-    //         Console.WriteLine($"{task.Id}. [{(task.Completed ? "X" : " ")}] {task.Description}");
-    //     }
-    // }
-
-void DisplayTasks(IMyCollection<TaskItem> tasks)
-{
-    Console.Clear();
-
-    int width = 50;
-    string lijn = new string('═', width);
-
-    // headers
-    Console.ForegroundColor = ConsoleColor.Cyan;
-    Console.WriteLine("╔" + lijn + "╗");
-    string title = "TODO LIST";
-    int average_legte_header = (width - title.Length) / 2;
-    Console.WriteLine("║" + new string(' ', average_legte_header) + title +
-                      new string(' ', width - title.Length - average_legte_header) + "║");
-
-    Console.WriteLine("╠" + lijn + "╣");
-    Console.ResetColor();
-
-
-
-    // taken zelf geprint 
-    var iterator = tasks.GetIterator();
-
-    if (!iterator.HasNext())
+    private void FilterTasksMenu()
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        string emptyMessage = "No tasks have been added yet";
-        Console.WriteLine("║ " + emptyMessage.PadRight(width - 1) + "║");
-        Console.ResetColor();
-    }
+        var filteredTasks = FilterTasks.FiltersTasks(_service.GetAllTasks());
 
-    while (iterator.HasNext())
-    {
-        var task = iterator.Next();
-
-        string status_tasks = task.Completed ? "X" : " ";
-        
-
-        string Id_en_status = $"{task.Id,4}. [{status_tasks}] ";
-        string taak_descriptie = task.Description;
-
-        int descriptionwidth = width - 1;
-        int firstLineWidth = descriptionwidth - Id_en_status.Length;
-
-        string descriptie_na_bewerk = taak_descriptie.Length > firstLineWidth
-            ? taak_descriptie.Substring(0, firstLineWidth)
-            : taak_descriptie; // neem alles wat past anders neem alles. 
-
-        Console.WriteLine("║ " + (Id_en_status + descriptie_na_bewerk).PadRight(descriptionwidth) + "║"); // padright zorgt dat het hetzelfde is als die :16 enz maar dan makkelijker
-
-        taak_descriptie = taak_descriptie.Length > firstLineWidth
-            ? taak_descriptie.Substring(firstLineWidth)
-            : ""; // resterende tekst die nog over zou zijn geweest
-
-        // vervolg regels als tekst te lang is
-        while (taak_descriptie.Length > 0)
-        {
-            string part = taak_descriptie.Length > descriptionwidth
-                ? taak_descriptie.Substring(0, descriptionwidth)
-                : taak_descriptie;
-
-            Console.WriteLine("║ " + part.PadRight(descriptionwidth) + "║");
-
-            taak_descriptie = taak_descriptie.Length > descriptionwidth
-                ? taak_descriptie.Substring(descriptionwidth)
-                : "";
-        }
-
-        Console.ResetColor();
-    }
-
-    // grondstukkie
-    Console.ForegroundColor = ConsoleColor.Cyan;
-    Console.WriteLine("╚" + lijn + "╝");
-    Console.ResetColor();
-}
-
-int DisplayAndChooseTasks(IMyCollection<TaskItem> tasks)
-{
-    int select_index = 0;
-
-    while(true)
-    {
-    
+        // Toon gefilterde taken (zelfde display als in DisplayAndChooseTasks)
         Console.Clear();
-
-        int width = 50;
-        string lijn = new string('═', width);
-
-        // headers
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("╔" + lijn + "╗");
-        string title = "TODO LIST";
-        int average_legte_header = (width - title.Length) / 2;
-        Console.WriteLine("║" + new string(' ', average_legte_header) + title +
-                      new string(' ', width - title.Length - average_legte_header) + "║");
-
-        Console.WriteLine("╠" + lijn + "╣");
-        Console.ResetColor();
-
-
-
-        // taken zelf geprint 
-        var iterator = tasks.GetIterator();
-
+        Console.WriteLine("=== Filtered Tasks ===\n");
+        var iterator = filteredTasks.GetIterator();
         if (!iterator.HasNext())
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            string emptyMessage = "No tasks have been added yet";
-            Console.WriteLine("║ " + emptyMessage.PadRight(width - 1) + "║");
-            Console.ResetColor();
+            Console.WriteLine("No tasks found.");
+        }
+        else
+        {
+            while (iterator.HasNext())
+            {
+                var task = iterator.Next();
+                string status = task.Completed ? "X" : " ";
+                Console.WriteLine($"{task.Id}. [{status}] {task.Description} (Priority: {task.Priority}, Created: {task.CreationDate})");
+            }
         }
 
-        int i = 0;
-    
-        while (iterator.HasNext())
+        Console.WriteLine("\nPress any key to return to menu...");
+        Console.ReadKey();
+    }
+
+    int DisplayAndChooseTasks(IMyCollection<TaskItem> tasks)
+    {
+        int select_index = 0;
+
+        while(true)
         {
-            var task = iterator.Next();
-
-            string status_tasks = task.Completed ? "X" : " ";
         
+            Console.Clear();
 
-            string Id_en_status = $"{task.Id,4}. [{status_tasks}] ";
-            string taak_descriptie = task.Description;
+            int width = 50;
+            string lijn = new string('═', width);
 
-            int descriptionwidth = width - 1;
-            int firstLineWidth = descriptionwidth - Id_en_status.Length;
+            // headers
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("╔" + lijn + "╗");
+            string title = "TODO LIST";
+            int average_legte_header = (width - title.Length) / 2;
+            Console.WriteLine("║" + new string(' ', average_legte_header) + title +
+                        new string(' ', width - title.Length - average_legte_header) + "║");
 
-            string descriptie_na_bewerk = taak_descriptie.Length > firstLineWidth
-                ? taak_descriptie.Substring(0, firstLineWidth)
-                : taak_descriptie; // neem alles wat past anders neem alles. 
+            Console.WriteLine("╠" + lijn + "╣");
+            Console.ResetColor();
 
-            if(i == select_index)
+
+
+            // taken zelf geprint 
+            var iterator = tasks.GetIterator();
+
+            if (!iterator.HasNext())
             {
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("║ >" + (Id_en_status + descriptie_na_bewerk).PadRight(descriptionwidth) + "║"); // padright zorgt dat het hetzelfde is als die :16 enz maar dan makkelijker
+                Console.ForegroundColor = ConsoleColor.Red;
+                string emptyMessage = "No tasks have been added yet";
+                Console.WriteLine("║ " + emptyMessage.PadRight(width - 1) + "║");
                 Console.ResetColor();
             }
-            else
+
+            int i = 0;
+        
+            while (iterator.HasNext())
             {
-                Console.WriteLine("║ " + (Id_en_status + descriptie_na_bewerk).PadRight(descriptionwidth) + "║"); // padright zorgt dat het hetzelfde is als die :16 enz maar dan makkelijker      
+                var task = iterator.Next();
+
+                string status_tasks = task.Completed ? "X" : " ";
+            
+
+                string Id_en_status = $"{task.Id,4}. [{status_tasks}] ";
+                string taak_descriptie = task.Description;
+
+                int descriptionwidth = width - 1;
+                int firstLineWidth = descriptionwidth - Id_en_status.Length;
+
+                string descriptie_na_bewerk = taak_descriptie.Length > firstLineWidth
+                    ? taak_descriptie.Substring(0, firstLineWidth)
+                    : taak_descriptie; // neem alles wat past anders neem alles. 
+
+                if(i == select_index)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("║ >" + (Id_en_status + descriptie_na_bewerk).PadRight(descriptionwidth) + "║"); // padright zorgt dat het hetzelfde is als die :16 enz maar dan makkelijker
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("║ " + (Id_en_status + descriptie_na_bewerk).PadRight(descriptionwidth) + "║"); // padright zorgt dat het hetzelfde is als die :16 enz maar dan makkelijker      
+                }
+
+                taak_descriptie = taak_descriptie.Length > firstLineWidth
+                    ? taak_descriptie.Substring(firstLineWidth)
+                    : ""; // resterende tekst die nog over zou zijn geweest
+
+                // vervolg regels als tekst te lang is
+                while (taak_descriptie.Length > 0)
+                {
+                    string part = taak_descriptie.Length > descriptionwidth
+                        ? taak_descriptie.Substring(0, descriptionwidth)
+                        : taak_descriptie;
+
+                    Console.WriteLine("║ " + part.PadRight(descriptionwidth) + "║");
+
+                    taak_descriptie = taak_descriptie.Length > descriptionwidth
+                        ? taak_descriptie.Substring(descriptionwidth)
+                        : "";
+                }
+
+                i++;
+                Console.ResetColor();
             }
 
-            taak_descriptie = taak_descriptie.Length > firstLineWidth
-                ? taak_descriptie.Substring(firstLineWidth)
-                : ""; // resterende tekst die nog over zou zijn geweest
-
-            // vervolg regels als tekst te lang is
-            while (taak_descriptie.Length > 0)
-            {
-                string part = taak_descriptie.Length > descriptionwidth
-                    ? taak_descriptie.Substring(0, descriptionwidth)
-                    : taak_descriptie;
-
-                Console.WriteLine("║ " + part.PadRight(descriptionwidth) + "║");
-
-                taak_descriptie = taak_descriptie.Length > descriptionwidth
-                    ? taak_descriptie.Substring(descriptionwidth)
-                    : "";
-            }
-
-            i++;
+            // grondstukkie
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("╚" + lijn + "╝");
             Console.ResetColor();
-        }
-
-        // grondstukkie
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("╚" + lijn + "╝");
-        Console.ResetColor();
-        var key = Console.ReadKey(true);
-        if (key.Key == ConsoleKey.UpArrow)
-        {
-            select_index--;
-            if (select_index < 0)
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.UpArrow)
             {
-                select_index = tasks.Count - 1;
+                select_index--;
+                if (select_index < 0)
+                {
+                    select_index = tasks.Count - 1;
+                }
             }
-        }
-        else if (key.Key == ConsoleKey.DownArrow)
-        {
-            select_index++;
-            if (select_index >= tasks.Count)
+            else if (key.Key == ConsoleKey.DownArrow)
             {
-                select_index = 0;
+                select_index++;
+                if (select_index >= tasks.Count)
+                {
+                    select_index = 0;
+                }
             }
-        }
-        else if (key.Key == ConsoleKey.Enter)
-        {
-            var it = tasks.GetIterator();
-            TaskItem task = null;
-            for (int j = 0; j <= select_index; j++)
+            else if (key.Key == ConsoleKey.Enter)
             {
-                task = it.Next();   
+                var it = tasks.GetIterator();
+                TaskItem task = null;
+                for (int j = 0; j <= select_index; j++)
+                {
+                    task = it.Next();   
+                }
+                return task.Id;
             }
-            return task.Id;
-        }
-        else if (key.Key == ConsoleKey.Escape)
-        {
-            return 0;
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                return 0;
+            }
         }
     }
-}
 
 
     string Prompt(string prompt)
@@ -251,19 +177,10 @@ int DisplayAndChooseTasks(IMyCollection<TaskItem> tasks)
             main_options.Add("Update Task");
             main_options.Add("Toggle Task State");
             main_options.Add("List Tasks");
+            main_options.Add("Filter Tasks");
             main_options.Add("Exit");
         while (true)
         {
-            
-
-            // var main_options = new List<string>
-            // {
-            //     "Add Task",
-            //     "Remove Task",
-            //     "Toggle Task State",
-            //     "List Tasks",
-            //     "Exit"
-            // };
 
             int select_index = 0;
 
@@ -395,11 +312,14 @@ int DisplayAndChooseTasks(IMyCollection<TaskItem> tasks)
                     _service.ToggleTaskCompletion(toggleId);
                     break;
                 case 4:
-                    DisplayTasks(_service.GetAllTasks());
+                    DisplayAndChooseTasks(_service.GetAllTasks());
                     Console.WriteLine("\nPress any key to return to menu...");
                     Console.ReadKey();
                     break;
                 case 5:
+                    FilterTasksMenu();
+                    break;
+                case 6:
                     return;
             }
         }
---
+++ view/Filtertask.cs
@@ -0,0 +1,205 @@
+public static class FilterTasks
+{
+private static readonly MyArrayList<(string Name, int Rank)> PriorityRanking;
+
+static FilterTasks()
+{
+    PriorityRanking = new MyArrayList<(string, int)>();
+    PriorityRanking.Add(("must have", 3));
+    PriorityRanking.Add(("should have", 2));
+    PriorityRanking.Add(("could have", 1));
+}
+    public static IMyCollection<TaskItem> FiltersTasks(IMyCollection<TaskItem> tasks)
+    {
+        IMyCollection<TaskItem> filteredTasks = tasks;
+
+        var filterOptions = new MyArrayList<string>();
+        filterOptions.Add("Status");
+        filterOptions.Add("Priority");
+        filterOptions.Add("Creation Date");
+        filterOptions.Add("Back");
+
+        int selectedFilter = 0;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Filter Tasks ===\n");
+            DisplayMenu(filterOptions, selectedFilter);
+
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                selectedFilter--;
+                if (selectedFilter < 0) selectedFilter = filterOptions.Count - 1;
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                selectedFilter++;
+                if (selectedFilter >= filterOptions.Count) selectedFilter = 0;
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                if (selectedFilter == filterOptions.Count - 1) return filteredTasks; // Back
+
+                switch (selectedFilter)
+                {
+                    case 0:
+                        filteredTasks = FilterByStatus(filteredTasks);
+                        break;
+                    case 1:
+                        filteredTasks = FilterByPriority(filteredTasks);
+                        break;
+                    case 2:
+                        filteredTasks = SortByCreationDate(filteredTasks);
+                        break;
+                }
+
+                // Toon resultaten
+                Console.Clear();
+                Console.WriteLine("=== Filtered Tasks ===\n");
+                DisplayTasks(filteredTasks);
+
+                Console.WriteLine("\nPress any key to return to filter menu...");
+                Console.ReadKey();
+            }
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                return filteredTasks;
+            }
+        }
+    }
+
+    private static IMyCollection<TaskItem> FilterByStatus(IMyCollection<TaskItem> tasks)
+    {
+        var statusOptions = new MyArrayList<string>();
+        statusOptions.Add("Finished");
+        statusOptions.Add("Unfinished");
+        statusOptions.Add("Back");
+
+        int selection = ChooseOption("Select Status:", statusOptions);
+        if (selection == 2) return tasks; // Back
+
+        bool completedFilter = selection == 0;
+        return tasks.Filter(t => t.Completed == completedFilter);
+    }
+
+    private static IMyCollection<TaskItem> FilterByPriority(IMyCollection<TaskItem> tasks)
+    {
+        var priorityOptions = new MyArrayList<string>();
+        priorityOptions.Add("Most important");
+        priorityOptions.Add("Least important");
+        priorityOptions.Add("Back");
+
+        int selection = ChooseOption("Select Priority:", priorityOptions);
+        if (selection == 2) return tasks; // Back
+
+        tasks.Sort((a, b) =>
+        {
+            int rankA = GetPriorityRank(a.Priority);
+            int rankB = GetPriorityRank(b.Priority);
+            return selection == 0 ? rankB - rankA : rankA - rankB;
+        });
+
+        return tasks;
+    }
+
+    private static int GetPriorityRank(string priority)
+    {
+        var iterator = PriorityRanking.GetIterator();
+        while (iterator.HasNext())
+        {
+            var p = iterator.Next();
+            if (p.Name == priority) return p.Rank;
+        }
+        return 0;
+    }
+
+    private static IMyCollection<TaskItem> SortByCreationDate(IMyCollection<TaskItem> tasks)
+    {
+        var dateOptions = new MyArrayList<string>();
+        dateOptions.Add("Ascending");
+        dateOptions.Add("Descending");
+        dateOptions.Add("Back");
+
+        int selection = ChooseOption("Select Creation Date Order:", dateOptions);
+        if (selection == 2) return tasks; // Back
+
+        tasks.Sort((a, b) =>
+            selection == 0 ? a.CreationDate.CompareTo(b.CreationDate)
+                            : b.CreationDate.CompareTo(a.CreationDate));
+
+        return tasks;
+    }
+
+    private static int ChooseOption(string title, IMyCollection<string> options)
+    {
+        int selectedIndex = 0;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"=== {title} ===\n");
+            DisplayMenu(options, selectedIndex);
+
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                selectedIndex--;
+                if (selectedIndex < 0) selectedIndex = options.Count - 1;
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                selectedIndex++;
+                if (selectedIndex >= options.Count) selectedIndex = 0;
+            }
+            else if (key.Key == ConsoleKey.Enter)
+            {
+                return selectedIndex;
+            }
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                return options.Count - 1; // Back
+            }
+        }
+    }
+
+    private static void DisplayMenu(IMyCollection<string> options, int selectedIndex)
+    {
+        var iterator = options.GetIterator();
+        int i = 0;
+        while (iterator.HasNext())
+        {
+            string option = iterator.Next();
+            if (i == selectedIndex)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine($"> {option}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"  {option}");
+            }
+            i++;
+        }
+    }
+
+    private static void DisplayTasks(IMyCollection<TaskItem> tasks)
+    {
+        var iterator = tasks.GetIterator();
+        if (!iterator.HasNext())
+        {
+            Console.WriteLine("No tasks found.");
+            return;
+        }
+
+        while (iterator.HasNext())
+        {
+            var task = iterator.Next();
+            string status = task.Completed ? "X" : " ";
+            Console.WriteLine($"{task.Id}. [{status}] {task.Description} (Priority: {task.Priority}, Created: {task.CreationDate})");
+        }
+    }
+}
