e24d2340dd4cab73c7659975d4fd8ba8ad12f0a1
mody963 <1083107@hr.nl>
2026-03-05 12:06:23 +0100

    fixed taskallocation and made an extra json file for it

--- model/task_allocation.cs
+++ model/task_allocation.cs
@@ -1,6 +1,8 @@
-public class Task_Allocation
+public class Task_Allocation : IEquatable<Task_Allocation> 
 {
     public int Task_Id{get;}
     public int Person_Id{get;}
-    
+    public bool Equals(Task_Allocation? other) => other is not null && other.Task_Id == Task_Id && other.Person_Id == Person_Id;
+
+    public override bool Equals(object? obj) => Equals(obj as Task_Allocation);
 } //
