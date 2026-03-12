84582afdca98b4b8b31ea5e5e9bbff1265d0170a
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-03-12 13:44:47 +0100

    betere linked listr met alvast wat comments van hoe ik alles aan wil gaan passen na research.

--- collections/MyLinkedList.cs
+++ collections/MyLinkedList.cs
@@ -1,40 +1,107 @@
-// 
+// find should find, delete should only delete so find is an helper method. 
 
-class LinkedList<T>: IMyLinkedList<T>
+class LinkedList<T>: IMyCollection<T>
 {
-    public interface IMyLinkedList<T> 
-{
+    private T FirstNode = default(T);
+    private T LastNode = default(T);
 
-    public class LinkedList<T>
+
+    // je moet ook de keuzen krijgen om een legen linked list aan te maken. 
+    public LinkedList()
     {
-        // This LinkedList is a doubly-Linked circular list.
-        internal LinkedListNode<T>? head;
-        internal int count;
-        internal int version;
-
-
-        // names for serialization
-        private const string VersionName = "Version"; // Do not rename (binary serialization)
-        private const string CountName = "Count"; // Do not rename (binary serialization)
-        private const string ValuesName = "Data";
-    public int Count
-        {
-            get { return count; }
-        }
-
-    public LinkedListNode<T>? First
-        {
-            get { return head; }
-        }
-
-    public LinkedListNode<T>? Last
-            {
-                
-            }
+    }
     
-    //IEnumerator<T> GetEnumerator();
-    //IMyEnumerator<T> GetMyEnumerator();
-}
+
+    // uiteindelijk ook de keuzen om een gevulde linked list aan te maken. 
+    // public LinkedList(hoeveel items en de items zelf)
+    // kijken of hoeveelheid items null is zo ja dan argument exception.
+    // via de add de items toevoegen. 
 
 
+    public void Add(T item) // add wordt gebruikt voor dingen aan het einde toevoegen 
+    {
+        return;
+    }
+
+    public void Insert(T item, int index)// 
+    {
+        
+    }
+
+
+    public void Remove(T item) // hierbij geeft het gewoon de naam van de item mee en dan pakt hij het meteen en delete hij het. 
+    {
+        return;
+    }
+    
+
+    public T? FindBy<K>(K key, Func<T, K, bool> comparer)
+    {
+        if (comparer == null)
+        {
+            throw new ArgumentNullException(nameof(comparer));
+        }
+        
+        // for (int i = 0; i < _count; i++)
+        // {
+        //     if (comparer(_items[i], key)) return _items[i];
+        // }
+            
+        return default(T);
+    }
+
+
+    public IMyCollection<T> Filter(Func<T, bool> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public void Sort(Comparison<T> comparison)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public int Count { get; }
+    
+    
+    public bool Dirty {get;} // was get set maar ik heb alleen get van gemaakt, omdat set private moet zijn
+    
+    
+    public T Reduce(Func<T, T, T> accumulator)
+    {
+        throw new NotImplementedException();
+    }
+    // or
+    
+    
+    public R Reduce<R>(R initial, Func<R, T, R> accumulator)
+    {
+        throw new NotImplementedException();
+    }
+    //or
+    
+    
+    public RResult Reduce<R, RResult>(R initial, Func<R, T, R> accumulator, Func<R, RResult> resultSelector)
+    {
+        throw new NotImplementedException();
+    }
+    
+
+    public IMyIterator<T> GetIterator()
+    {
+        throw new NotImplementedException();
+    }
+    public T[] ToArray()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
 }
+
