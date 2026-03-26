98ba044d329b5e09f598bc84a0570bead35a819c
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-03-26 13:22:49 +0100

    doubly linked list.

--- collections/MyLinkedList.cs
+++ collections/MyLinkedList.cs
@@ -13,6 +13,8 @@ class MyLinkedList<T>: IMyCollection<T>
         // adress
         public Node<T>? Next;
 
+        public Node<T>? Previous; // voor doubly referencie naar de vorrige. 
+
         public Node(T data)
         {
             Data = data;
@@ -72,6 +74,7 @@ class MyLinkedList<T>: IMyCollection<T>
         else
         {
             _tail.Next = newNode; // de oude tail wordt de nieuwe node met info. de head verander je niet. 
+            newNode.Previous = _tail; // neiuwe node naar oude tail. 
             _tail = newNode;
         }
 
@@ -87,10 +90,12 @@ class MyLinkedList<T>: IMyCollection<T>
         }
         Node<T> newNode = new Node<T>(item);
         newNode.Next = _head; // hierbij verwijst de nieuwe node naar de oude head. en dus komt het ervoor. 
+        if (_head != null)
+            _head.Previous = newNode;
         _head = newNode; // hierna vervangt de nieuwe node de oude head.
 
         if (_tail == null)
-        _tail = newNode;
+            _tail = newNode;
 
         _count++;
         Dirty = true;
@@ -130,6 +135,11 @@ class MyLinkedList<T>: IMyCollection<T>
         }
 
         newNode.Next = current.Next; // de nieuwe node wijst nu naar het adress waar de vorige naar wees. 
+        newNode.Previous = current; // de pev die wordt dus de current want dat was de oude. 
+
+        if (current.Next != null)
+            current.Next.Previous = newNode; // doubly linked list update
+
         current.Next = newNode; // het adress van de current node wijst nu naar de nieuwe. 
 
         if (newNode.Next == null)
@@ -148,6 +158,8 @@ class MyLinkedList<T>: IMyCollection<T>
         if (_head.Data.Equals(item))
         {
             _head = _head.Next;
+            if (_head != null)
+                _head.Previous = null; // a;s head nie null is dan moey je hey wel leeg maken. 
 
             if (_head == null)
                 _tail = null;
@@ -162,7 +174,11 @@ class MyLinkedList<T>: IMyCollection<T>
         {
             if (current.Next.Data.Equals(item)) // mag niet dezelfde data hebben vandaar de equals. 
             {
-                current.Next = current.Next.Next;
+                Node<T>? nodeToRemove = current.Next;
+                current.Next = nodeToRemove.Next;
+
+                if (nodeToRemove.Next != null)
+                    nodeToRemove.Next.Previous = current;
 
                 if (current.Next == null)
                     _tail = current;
