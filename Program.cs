2c84c9bbaa6fc34f3cc67bb30caff86b809521ca
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-03-30 14:01:48 +0200

    aanpassingen door saleem

--- Program.cs
+++ Program.cs
@@ -3,18 +3,37 @@
     static void Main(string[] args)
     {
         // Dependency injection: wiring up our components
-        string filePath = "tasks.json";
-        string filePath2 = "Persons.json";
-        string filePath3 = "Allocations.json";
+        string filepath_task = "tasks.json";
+        string filepath_persons = "Persons.json";
+        string filepath_allocations = "Allocations.json";
         //collection
-        ICollectionFactory<TaskItem> taskFactory = new MyArrayListFactory<TaskItem>();
-        ICollectionFactory<Person> personFactory = new MyArrayListFactory<Person>();
-        ICollectionFactory<Task_Allocation> allocationFactory = new MyArrayListFactory<Task_Allocation>();
+        ICollectionFactory<TaskItem> taskFactory;
+        ICollectionFactory<Person> personFactory;
+        ICollectionFactory<Task_Allocation> allocationFactory;
 
+        switch(args[0])
+        {
+            case "arraylist":
+                taskFactory = new MyArrayListFactory<TaskItem>();
+                personFactory = new MyArrayListFactory<Person>();
+                allocationFactory = new MyArrayListFactory<Task_Allocation>();
+                break;
+            case "linkedlist":
+                taskFactory = new MyLinkedListFactory<TaskItem>();
+                personFactory = new MyLinkedListFactory<Person>();
+                allocationFactory = new MyLinkedListFactory<Task_Allocation>();
+                break;
+            default:
+                Console.WriteLine("Invalid collection type specified. Defaulting to ArrayList.");
+                taskFactory = new MyArrayListFactory<TaskItem>();
+                personFactory = new MyArrayListFactory<Person>();
+                allocationFactory = new MyArrayListFactory<Task_Allocation>();
+                break;
+        }
 
-        ITaskRepository repository = new TaskRepository(filePath, taskFactory);
-        IPersonRepository repository2 = new PersonRepository(filePath2, personFactory);
-        IAllocationRepository repository3 = new AllocationRepository(filePath3, allocationFactory);
+        ITaskRepository repository = new TaskRepository(filepath_task, taskFactory);
+        IPersonRepository repository2 = new PersonRepository(filepath_persons, personFactory);
+        IAllocationRepository repository3 = new AllocationRepository(filepath_allocations, allocationFactory);
 
 
         ITaskService service = new TaskService(repository);
