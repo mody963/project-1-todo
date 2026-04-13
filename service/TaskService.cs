dde711afdb600f74fe815e0f41da550cd0d1dabf
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-04-13 13:31:15 +0200

    hjk,jhgvbnm,

--- service/TaskService.cs
+++ service/TaskService.cs
@@ -16,47 +16,6 @@ class TaskService : ITaskService
     //public IEnumerable<TaskItem> GetAllTasks() => _tasks;
     public IMyCollection<TaskItem> GetAllTasks() => _tasks;
 
-
-    // public void AddTask(string description)
-    // {
-    //     int newId = _tasks.Count > 0
-    //         ? _tasks[_tasks.Count - 1].Id + 1
-    //         : 1;
-
-    //     var newTask = new TaskItem
-    //     {
-    //         Id = newId,
-    //         Description = description,
-    //         Completed = false
-    //     };
-
-    //     _tasks.Add(newTask);
-    //     _repository.SaveTasks(_tasks);
-    // }
-    // public void AddTask(string description, string priority, TaskItem chosenTask)
-    // {
-    //     int newId = 1;
-    //     var iterator = _tasks.GetIterator();
-    //     while (iterator.HasNext())
-    //     {
-    //         var task = iterator.Next();
-    //         if (task.Id >= newId)
-    //             newId = task.Id + 1;
-    //     }
-    //     var newTask = new TaskItem
-    //     {
-    //         Id = newId,
-    //         Description = description,
-    //         Priority = priority,
-    //         Status = "to do",
-    //         CreationDate = DateTime.Now,
-    //         dependant = chosenTask
-    //     };
-
-    //     _tasks.Add(newTask);
-    // }
-
-
     // Aimee
     public void AddTask(string description, string priority, TaskItem chosenTask)
     {
