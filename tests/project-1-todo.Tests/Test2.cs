2ccacce9e7c1564d7d4728f2df791dc6e34f1d01
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-05-05 15:38:44 +0200

    laatste

--- tests/project-1-todo.Tests/Test2.cs
+++ tests/project-1-todo.Tests/Test2.cs
@@ -72,5 +72,132 @@ public sealed class Test2
         Assert.AreEqual(3, result[2]);
     }
 
- 
+    [TestMethod]
+    public void TestIterator()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // act
+        var iterator = list.GetIterator();
+
+        // assert
+        Assert.IsTrue(iterator.HasNext());
+        Assert.AreEqual(1, iterator.Next());
+        Assert.IsTrue(iterator.HasNext());
+        Assert.AreEqual(2, iterator.Next());
+        Assert.IsTrue(iterator.HasNext());
+        Assert.AreEqual(3, iterator.Next());
+        Assert.IsFalse(iterator.HasNext());
+    }
+
+    [TestMethod]
+    public void TestFilter()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+
+        // act
+        var evenItems = list.Filter(x => x % 2 == 0);
+        var result = evenItems.ToArray();
+
+        // assert
+        Assert.AreEqual(2, evenItems.Count);
+        Assert.AreEqual(2, result[0]);
+        Assert.AreEqual(4, result[1]);
+    }
+
+    [TestMethod]
+    public void TestSort()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(3);
+        list.Add(1);
+        list.Add(4);
+        list.Add(2);
+
+        // act
+        list.Sort((a, b) => a.CompareTo(b));
+        var result = list.ToArray();
+
+        // assert
+        Assert.AreEqual(1, result[0]);
+        Assert.AreEqual(2, result[1]);
+        Assert.AreEqual(3, result[2]);
+        Assert.AreEqual(4, result[3]);
+    }
+
+    [TestMethod]
+    public void TestReduce()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // act
+        var sum = list.Reduce((a, b) => a + b);
+
+        // assert
+        Assert.AreEqual(6, sum);
+    }
+
+    [TestMethod]
+    public void TestToArray()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // act
+        var result = list.ToArray();
+
+        // assert
+        Assert.AreEqual(3, result.Length);
+        Assert.AreEqual(1, result[0]);
+        Assert.AreEqual(2, result[1]);
+        Assert.AreEqual(3, result[2]);
+    }
+
+    [TestMethod]
+    public void TestTryFindBy()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // act
+        var found = list.TryFindBy(2, (data, key) => data.CompareTo(key), out int value);
+
+        // assert
+        Assert.IsTrue(found);
+        Assert.AreEqual(2, value);
+    }
+
+    [TestMethod]
+    public void TestResetDirty()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+
+        // act
+        list.ResetDirty();
+
+        // assert
+        Assert.IsFalse(list.Dirty);
+    }
 }
