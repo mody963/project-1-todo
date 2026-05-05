ef1480243afe817482b9847f73d0f52f4ef424ad
B1ngB0ngL1ng <aimeemolendijk1@gmail.com>
2026-05-05 15:21:18 +0200

    verder

--- tests/project-1-todo.Tests/Test2.cs
+++ tests/project-1-todo.Tests/Test2.cs
@@ -36,4 +36,41 @@ public sealed class Test2
 
     [TestMethod]
 
+
+        public void TestRemoveItem()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        // act
+        list.Remove(2);
+
+        // assert
+        Assert.AreEqual(2, list.Count);
+        Assert.IsTrue(list.Dirty);
+    }
+
+    [TestMethod]
+    public void TestInsertAtIndex()
+    {
+        // arrange
+        var list = new MyLinkedList<int>();
+        list.Add(1);
+        list.Add(3);
+
+        // act
+        list.Insert(2, 1);
+        var result = list.ToArray();
+
+        // assert
+        Assert.AreEqual(3, list.Count);
+        Assert.AreEqual(1, result[0]);
+        Assert.AreEqual(2, result[1]);
+        Assert.AreEqual(3, result[2]);
+    }
+
+ 
 }
